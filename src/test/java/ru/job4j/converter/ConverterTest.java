2d692f745d8bf6e73884c91598610e37955314cc
Serg0n <cyppe135@gmail.com>
2024-10-17 00:31:38 +0300

    1. Модульные тесты [#505277 #517819]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -13,7 +13,6 @@ class ConverterTest {
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
-
     @Test
     void whenConvert180RblThen3Dollar() {
         float input = 225;
