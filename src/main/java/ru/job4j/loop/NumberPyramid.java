bf5b4ec12d5206d67a88632b55fbfa6786268107
Serg0n <cyppe135@gmail.com>
2024-10-19 22:27:17 +0300

    Числовая пирамида [#505218]

---
+++ src/main/java/ru/job4j/loop/NumberPyramid.java
@@ -0,0 +1,25 @@
+package ru.job4j.loop;
+
+public class NumberPyramid {
+    public static void draw(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k);
+            }
+            for (int l = i - 1; l >= 1; l--) {
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Number pyramid of height 3:");
+        draw(3);
+        System.out.println("Number pyramid of height 5:");
+        draw(5);
+    }
+}
