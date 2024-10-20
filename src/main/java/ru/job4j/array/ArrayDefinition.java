b1917b1cdfc5e8aa8375d89417bab982f7028aa0
Serg0n <cyppe135@gmail.com>
2024-10-20 12:47:52 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        names[0] = "Сергей";
+        names[1] = "Александр";
+        names[2] = "Дмитрий";
+        names[3] = "Василий";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
