ee69d8c45a4891b99396b2e381efeaefa4958328
Serg0n <cyppe135@gmail.com>
2024-10-25 10:55:04 +0300

    6.7.1. Моно строка в матрице. [#505245 #518857]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -1,42 +1,28 @@
 package ru.job4j.array;
-
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-
 public class MatrixCheckTest {
     @Test
-    public void whenDiagonalFullX() {
+    public void whenHasMonoHorizontal() {
         char[][] input = {
-                {'X', ' ', ' '},
-                {' ', 'X', ' '},
-                {' ', ' ', 'X'},
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
         };
-        char[] result = MatrixCheck.extractDiagonal(input);
-        char[] expected = {'X', 'X', 'X'};
-        assertThat(result).containsExactly(expected);
+        int row = 1;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
+        assertThat(result).isTrue();
     }
 
     @Test
-    public void whenDiagonalFullOne() {
+    public void whenHasNOTMonoHorizontal() {
         char[][] input = {
-                {'1', ' ', ' '},
-                {' ', '1', ' '},
-                {' ', ' ', '1'},
+                {'X', ' ', 'X'},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
         };
-        char[] result = MatrixCheck.extractDiagonal(input);
-        char[] expected = {'1', '1', '1'};
-        assertThat(result).containsExactly(expected);
-    }
-
-    @Test
-    public void whenDiagonalMix() {
-        char[][] input = {
-                {'X', ' ', ' '},
-                {' ', 'Y', ' '},
-                {' ', ' ', 'Z'},
-        };
-        char[] result = MatrixCheck.extractDiagonal(input);
-        char[] expected = {'X', 'Y', 'Z'};
-        assertThat(result).containsExactly(expected);
+        int row = 0;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
+        assertThat(result).isFalse();
     }
 }
